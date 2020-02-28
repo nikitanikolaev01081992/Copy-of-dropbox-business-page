fc7f3c7950221b1d1aeda451de6e8f174f55c24d
nikitanikolaev <nikitanikolaev01081992@gmail.com>
2020-02-28 04:50:00 +0300

    finished section plans

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+// let header = document.body.getElementsByTagName("header")[0];
+// let button = document.body.querySelector(".button-try");
+
+// window.addEventListener("scroll", (event) => {
+//     header.classList.add("header-white");
+//     button.classList.add("button-try-blue");
+// });
