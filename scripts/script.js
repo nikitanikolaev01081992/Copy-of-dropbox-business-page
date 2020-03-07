095bbc512e4d8e11353e3a205ed63c3127ef0507
nikitanikolaev <nikitanikolaev01081992@gmail.com>
2020-03-07 05:10:30 +0300

    added draft of event handler
    handler for added for buttons in section FAQ

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,48 @@
-// let header = document.body.getElementsByTagName("header")[0];
-// let button = document.body.querySelector(".button-try");
+class FaqQuestion {
+    constructor(domQuestion, cssClass, domText, domButton) {
+        this.domLink = domQuestion;
+        this.cssClass = cssClass;
+        this.text = new Text(domText, this);
+        this.button = new Button(domButton, this);
+        
+    }
 
-// window.addEventListener("scroll", (event) => {
-//     header.classList.add("header-white");
-//     button.classList.add("button-try-blue");
-// });
+}
+
+
+class Button {
+
+    constructor(button, container) {
+        this.domLink = button;
+        this.container = container;
+
+        this.domLink.addEventListener("click", (evt) => {
+            this.handleClick(evt, this.container);
+        });
+    }
+
+    handleClick(evt, container) {
+        container.text.handleEvent();
+    }
+
+
+}
+
+class Text {
+    constructor(button, container) {
+        this.domLink = button;
+        this.container = container;
+    }
+
+    handleEvent() {
+        this.domLink.classList.toggle(this.container.cssClass);
+    }
+}
+
+
+let faq1 = new FaqQuestion(
+            document.querySelectorAll(".faq-item")[0],
+            "faq-item__text_active",
+            document.querySelectorAll(".faq-item__text")[0],
+            document.querySelectorAll(".faq-item__button")[0],
+);
