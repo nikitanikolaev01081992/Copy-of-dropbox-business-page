492b27f9876db734976eb3e98f6567379e118a87
nikitanikolaev <nikitanikolaev01081992@gmail.com>
2020-03-12 20:19:40 +0300

    working on issue with event click

--- scripts/script.js
+++ scripts/script.js
@@ -1,48 +1,42 @@
 class ListOfFaqQuestions {
     list = [];
-    
+
     constructor(domObject, arrayOfQuestions) {
         this.domObject = domObject;
         this.list = Array.prototype.map.call(arrayOfQuestions, element => {
             return new FaqQuestion(element, "faq-item__text_active");
         });
-        
-        
-        this.domObject.addEventListener("click", (evt) => {
+
+        this.domObject.addEventListener("click", evt => {
             this.handleClick(evt);
-        })
+        });
     }
 
     handleClick(event) {
         this.list.forEach(element => {
-            if (event.currentTarget !== element.button.domLink) {
+            if (event.target !== element.button.domLink) {
                 element.text.removeClass();
             }
         });
-        
     }
 }
-
 
 class FaqQuestion {
     constructor(domQuestion, cssClass) {
         this.domLink = domQuestion;
         this.cssClass = cssClass;
 
-        this.text = new Text(document.querySelector(".faq-item").querySelector(".faq-item__text"), this);
-        this.button = new Button(document.querySelector(".faq-item").querySelector(".faq-item__button"), this);
+        this.text = new Text(this.domLink.querySelector(".faq-item__text"), this);
+        this.button = new Button(this.domLink.querySelector(".faq-item__button"), this);
     }
-
 }
 
-
 class Button {
-
     constructor(button, container) {
         this.domLink = button;
         this.container = container;
 
-        this.domLink.addEventListener("click", (evt) => {
+        this.domLink.addEventListener("click", evt => {
             this.handleClick(evt, this.container);
         });
     }
@@ -50,8 +44,6 @@ class Button {
     handleClick(evt, container) {
         container.text.handleEvent();
     }
-
-
 }
 
 class Text {
@@ -69,10 +61,4 @@ class Text {
     }
 }
 
-
-
-
 let listOfFaqQuestions = new ListOfFaqQuestions(document.querySelector(".faq-questions"), document.querySelectorAll(".faq-item"));
-
-
-
